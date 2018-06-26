d32c59d59786390ec8804e92f421e565498cf630
rzonsol <>
2018-06-26 22:46:39 +0200

    Initial commit

---
+++ src/main/java/pl/rzonsol/springboot/rest/webservice/example/RestwebserviceApplication.java
@@ -0,0 +1,12 @@
+package pl.rzonsol.springboot.rest.webservice.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestwebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestwebserviceApplication.class, args);
+	}
+}
