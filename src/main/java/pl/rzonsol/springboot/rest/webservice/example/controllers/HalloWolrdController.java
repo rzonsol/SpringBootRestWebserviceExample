ecc77a99628be998ab93a914cec58683cff2c3ac
rzonsol <>
2018-06-27 23:12:40 +0200

    add debugging and path var

--- src/main/java/pl/rzonsol/springboot/rest/webservice/example/controllers/HalloWolrdController.java
+++ src/main/java/pl/rzonsol/springboot/rest/webservice/example/controllers/HalloWolrdController.java
@@ -2,6 +2,7 @@ package pl.rzonsol.springboot.rest.webservice.example.controllers;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import pl.rzonsol.springboot.rest.webservice.example.dto.HelloWorldDto;
 
@@ -16,5 +17,10 @@ public class HalloWolrdController {
     @GetMapping(path = "/hello-world-dto")
     public HelloWorldDto helloWorldDto(){
         return new HelloWorldDto("Hello World!");
+    }
+
+    @GetMapping(path = "/hello-world-path-var/{name}")
+    public HelloWorldDto helloWorldDto(@PathVariable String name){
+        return new HelloWorldDto(String.format("Hello World: %s", name));
     }
 }
