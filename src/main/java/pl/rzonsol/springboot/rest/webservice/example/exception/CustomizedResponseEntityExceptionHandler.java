110dd80ade0d92df122cdd29e3970cd57c2085c5
rzonsol <rzonsol@gmail.com>
2018-07-09 22:48:07 +0200

    customize exception response structure

---
+++ src/main/java/pl/rzonsol/springboot/rest/webservice/example/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,26 @@
+package pl.rzonsol.springboot.rest.webservice.example.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handlerAllExceptions(Exception exception, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(exception.getMessage(), request.getDescription(false));
+        return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<Object> handlerUserNotFoundException(UserNotFoundException exception, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(exception.getMessage(), request.getDescription(false));
+        return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+}
---
+++ src/main/java/pl/rzonsol/springboot/rest/webservice/example/exception/ExceptionResponse.java
@@ -0,0 +1,24 @@
+package pl.rzonsol.springboot.rest.webservice.example.exception;
+
+import lombok.Getter;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Date;
+
+@Getter
+public class ExceptionResponse {
+
+    private final Date time;
+    private final String message;
+    private final String details;
+
+    public ExceptionResponse(String message) {
+        this(message, StringUtils.EMPTY);
+    }
+
+    public ExceptionResponse(String message, String details) {
+        this.time = new Date();
+        this.message = message;
+        this.details = details;
+    }
+}
