1d0220ce6aa0575f4d3b5240784be31b7d5a6acf
rzonsol <rzonsol@gmail.com>
2018-07-17 06:58:07 +0200

    add swagger2

---
+++ src/main/java/pl/rzonsol/springboot/rest/webservice/example/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package pl.rzonsol.springboot.rest.webservice.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api(){
+        return new Docket(SWAGGER_2);
+    }
+
+}
