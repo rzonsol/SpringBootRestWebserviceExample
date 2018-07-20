2f5fe32fcd1087eb1e7f4b5c3fc15bc0979697f2
rzonsol <rzonsol@gmail.com>
2018-07-20 17:23:02 +0200

    add documentation to api with swagger annotation

--- src/main/java/pl/rzonsol/springboot/rest/webservice/example/config/SwaggerConfig.java
+++ src/main/java/pl/rzonsol/springboot/rest/webservice/example/config/SwaggerConfig.java
@@ -2,18 +2,32 @@ package pl.rzonsol.springboot.rest.webservice.example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
+    public static final Contact DEFAULT_CONTACT = new Contact("", "", "");
+    private static final springfox.documentation.service.ApiInfo API_INFO = new ApiInfo("title",
+            "String description",
+            "String version",
+            "String termsOfServiceUrl",
+            DEFAULT_CONTACT,
+            "String license",
+            "String licenseUrl",
+            Collections.emptyList());
+
     @Bean
     public Docket api(){
-        return new Docket(SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(API_INFO);
     }
 
 }
--- src/main/java/pl/rzonsol/springboot/rest/webservice/example/entity/User.java
+++ src/main/java/pl/rzonsol/springboot/rest/webservice/example/entity/User.java
@@ -1,5 +1,7 @@
 package pl.rzonsol.springboot.rest.webservice.example.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,13 +15,16 @@ import java.util.Date;
 @AllArgsConstructor
 @Builder
 @NoArgsConstructor
+@ApiModel(description = "User model description!")
 public class User {
 
     private Integer id;
 
     @Size(min = 2, message = "Name should have at least 2 character!")
+    @ApiModelProperty(notes = "Name should have at least 2 characters!")
     private String name;
 
     @Past
+    @ApiModelProperty(notes = "Date should be in the past!")
     private Date birthDate;
 }
